32d16a7b7747b6feb4e7adb40791b8202613ba98
Spencer Weaver <sweaver@eventideinvestments.com>
2023-04-12 14:21:54 -0400

    built the CardList component

--- app/monsters-rolodex/src/App.js
+++ app/monsters-rolodex/src/App.js
@@ -1,5 +1,7 @@
 import { Component } from 'react';
 
+import CardList from './components/card-list/card-list.component'
+
 // import logo from './logo.svg';
 import './App.css';
 
@@ -27,36 +29,46 @@ class App extends Component {
     }))
   }
 
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    console.log(event.target.value);
+    this.setState(() => {
+      return { searchField }
+    },
+    () => {
+      console.log({ endingArray: this.state.monsters });
+    })
+  }
+
   render() {
     console.log('render() -> 2')
 
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    const { monsters, searchField } = this.state
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
     });
 
     return (
       <div className="App">
-        <input className='search-box' type='search' placeholder='search monsters' onChange={(event) => {
-          const searchField = event.target.value.toLocaleLowerCase();
-          console.log(event.target.value);
+        <input
+          className='search-box'
+          type='search'
+          placeholder='search monsters'
+          onChange={onSearchChange}
+        />
+
+      <CardList monsters={filteredMonsters} />
 
 
-
-          this.setState(() => {
-            return { searchField }
-          },
-          () => {
-            console.log({ endingArray: this.state.monsters });
-          })
-        }}/>
-        {
-          filteredMonsters.map((monster) => {
-            return (
-              <div key={monster.id}>
-                <h1>{monster.name}</h1>
-              </div>
-            )
-          })
+        { // filteredMonsters.map((monster) => {
+          //   return (
+          //     <div key={monster.id}>
+          //       <h1>{monster.name}</h1>
+          //     </div>
+          //   )
+          // })
         }
       </div>
     );
