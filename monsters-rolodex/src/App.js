02aa4f7cf6c49acc6e5107461624d7141b0f8927
Spencer Weaver <spencer@compassfoundation.io>
2023-12-27 17:38:45 -0500

    restart the monsters rolodex

---
+++ monsters-rolodex/src/App.js
@@ -0,0 +1,40 @@
+import { Component } from 'react';
+
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: 'Spencer Weaver'
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hi {this.state.name}
+          </p>
+          <button onClick={() => {
+            this.setState({name: 'Andrei'})
+          }}>Change me</button>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
