4ba1f33cf266f8f20c74d24fe9ef857250e53d06
Spencer Weaver <spencweaver@gmail.com>
2024-01-16 17:47:57 -0500

    use setState with callback

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
     super();
 
     this.state = {
-      name: 'Spencer Weaver'
+      name: {firstName: 'Spencer', lastName: 'Weaver'},
+      company: 'ZTM',
     }
   }
 
@@ -18,10 +19,16 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hi {this.state.name}
+            Hi {this.state.name.firstName} {this.state.name.lastName}
           </p>
           <button onClick={() => {
-            this.setState({name: 'Andrei'})
+            this.setState(() => {
+              return {
+                name: {firstName: 'Andrei', lastName: 'Neaogie'},
+              }
+            }, () => {
+              console.log(this.state)
+            })
           }}>Change me</button>
           <a
             className="App-link"
