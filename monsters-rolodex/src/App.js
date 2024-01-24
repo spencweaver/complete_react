8475ff177e644eded1232ffc07ebebaeb3289929
Spencer Weaver <spencweaver@gmail.com>
2024-01-24 17:34:18 -0500

    add key to div elements

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -8,37 +8,28 @@ class App extends Component {
     super();
 
     this.state = {
-      name: {firstName: 'Spencer', lastName: 'Weaver'},
-      company: 'ZTM',
+      monsters: [
+        {
+          name: 'Linda',
+          id: '12eriesn'
+        },
+        {
+          name: 'Frank',
+          id: '12eriesns'
+        }
+      ]
     }
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi {this.state.name.firstName} {this.state.name.lastName}
-          </p>
-          <button onClick={() => {
-            this.setState(() => {
-              return {
-                name: {firstName: 'Andrei', lastName: 'Neaogie'},
-              }
-            }, () => {
-              console.log(this.state)
-            })
-          }}>Change me</button>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {
+          this.state.monsters.map((monster) => {
+            return <div key={monster.id} >
+              <h1>{monster.name}</h1>
+              </div>
+          })}
       </div>
     );
   }
