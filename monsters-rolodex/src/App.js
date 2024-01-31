206211b4101ec046f992c82a2dc7e272fa3efda3
Spencer Weaver <spencweaver@gmail.com>
2024-01-31 17:59:08 -0500

    add input field for searching

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -26,6 +26,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <input className='search-box' type='search' placeholder='search monsters' onChange={(event) => {
+          const searchString = event.target.value.toLocaleLowerCase();
+          const filteredMonsters = this.state.monsters.filter((monster) => {
+            return monster.name.toLocaleLowerCase().includes(searchString);
+          })
+          this.setState(() => {
+            return { monsters: filteredMonsters }
+            });
+        }}
+        />
         {
           this.state.monsters.map((monster) => {
             return <div key={monster.id} >
