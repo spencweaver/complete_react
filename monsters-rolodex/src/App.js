d3820298ab84660a177ccdb97241abe403f62c45
Spencer Weaver <spencweaver@gmail.com>
2024-02-08 17:06:55 -0500

    create the card-list component

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -44,13 +44,8 @@ class App extends Component {
       <div className="App">
         <input className='search-box' type='search' placeholder='search monsters' onChange={onSearchChange}
         />
-        {/* {
-          filteredMonsters.map((monster) => {
-            return <div key={monster.id} >
-              <h1>{monster.name}</h1>
-              </div>
-          })} */}
-          <CardList />
+
+          <CardList monsters={ filteredMonsters }/>
       </div>
     );
   }
