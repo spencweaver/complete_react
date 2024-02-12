ce730b03906c1223c4b077bbe26714405a3cc808
Spencer Weaver <spencweaver@gmail.com>
2024-02-12 17:35:55 -0500

    create the search-box component

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 
 import CardList from './components/card-list/card-list.component'
+import SearchBox from './components/search-box/search-box.component'
 import './App.css';
 
 
@@ -42,10 +43,8 @@ class App extends Component {
 
     return (
       <div className="App">
-        <input className='search-box' type='search' placeholder='search monsters' onChange={onSearchChange}
-        />
-
-          <CardList monsters={ filteredMonsters }/>
+        <SearchBox onChangeHandler={onSearchChange} placeholder='search monsters' className='search-box' />
+        <CardList monsters={ filteredMonsters }/>
       </div>
     );
   }
