f776aa3d41b271f343536e47cb31be435cf43abd
Spencer Weaver <spencweaver@gmail.com>
2024-05-03 17:19:53 -0400

    begin using 'useState'

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 
 import CardList from './components/card-list/card-list.component'
 import SearchBox from './components/search-box/search-box.component'
@@ -6,11 +6,20 @@ import './App.css';
 
 
 const App = () => {
+
+  const [searchField, setSearchField] = useState('');
+
+  const onSearchChange = (event) => {
+          const searchFieldString = event.target.value.toLocaleLowerCase();
+          setSearchField(searchFieldString);
+        }
+
+
   return (
     <div className="App">
       <h1 className="app-title">Monsters Rolodex</h1>
-      {/* <SearchBox onChangeHandler={onSearchChange} placeholder='search monsters' className='monsters-search-box' />
-      <CardList monsters={ filteredMonsters }/> */}
+      <SearchBox onChangeHandler={onSearchChange} placeholder='search monsters' className='monsters-search-box' />
+      {/* <CardList monsters={ filteredMonsters }/> */}
     </div>
   );
 }
