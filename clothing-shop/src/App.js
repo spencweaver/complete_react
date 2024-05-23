b3d4b309450a0ee1e343d8f5799a06aa4f9d2bad
Spencer Weaver <spencweaver@gmail.com>
2024-05-23 11:46:49 -0400

    setup store categories

--- clothing-shop/src/App.js
+++ clothing-shop/src/App.js
@@ -1,13 +1,29 @@
 const App = () => {
+
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+    },
+    {
+      id: 2,
+      title: 'Shirts',
+    }
+  ]
   return (
     <div className="categories-container">
-      <div className="category-container">
-        <img />
-        <div className="category-body-container">
-          <h2>Hats</h2>
-          <p>Shop Now</p>
+      {categories.map(({title}) => (
+        <div className="category-container">
+          <img className='background-image' src='https://media.istockphoto.com/id/523633973/photo/golden-retriever-puppy.jpg?s=612x612&w=is&k=20&c=buTVqvXlrNTEXa0ywk98FsL0azTa9-YcoeCRJb3pwws=' />
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
         </div>
-      </div>
+      )
+
+      )}
+
     </div>
   );
 }
